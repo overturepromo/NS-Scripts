70cb9575ca5ea2ea9e3f7b10510b8f4cdcae9a7e
jacobg@overturepromo.com <jacobg@overturepromo.com>
2021-09-10 15:41:50 -0500

    payload complete

--- OP_SCHED_DDJapanOrders.js
+++ OP_SCHED_DDJapanOrders.js
@@ -4,313 +4,253 @@
  *Description: Scheduled Script to send Sales Orders to DoorDash Japan
  */
 
+
+
+/*
+High Level
+-Load a saved search
+-Loop through results
+-Load records
+-Create payload, send to third party. 
+-Send Payload
+-Evaluate response from third party, handle accordingly.
+*/
 function sendSO() {
 
-	//define header object for nlapiRequestURL() call
-	var header = {
-    'Cache-Control' : 'no-cache',
-		'Content-Type' : 'application/json',
-		'Authorization':'9661B173-E407-4793-9BC3-E7896554DAE6'
-	};
+  //define header object for nlapiRequestURL() call
+  var header = {
+    'Cache-Control': 'no-cache',
+    'Content-Type': 'application/json',
+    'Authorization': '9661B173-E407-4793-9BC3-E7896554DAE6'
+  };
 
-	//DoorDash Japan Orders to Send via Integration
-	var results = nlapiSearchRecord('salesorder','customsearch4722');
+  //DoorDash Japan Orders to Send via Integration
+  var results = nlapiSearchRecord('salesorder', 'customsearch4722');
 
-	if(results) {
+  if (results) {
 
-    var checkGovernance = function() {
+    var checkGovernance = function () {
 
-			var governanceThreshold = 500;
-			var context = nlapiGetContext();
+      var governanceThreshold = 500;
+      var context = nlapiGetContext();
 
-			if(context.getRemainingUsage() < governanceThreshold) {
+      if (context.getRemainingUsage() < governanceThreshold) {
 
-				try{
-					
-					var script = nlapiScheduleScript('customscript_op_sched_doordashaustralia');
+        try {
 
-          if(script == 'QUEUED') {
-            nlapiLogExecution('ERROR','Re-scheduling due to governance', 'Successful re-schedule.');
+          var script = nlapiScheduleScript('customscript_op_sched_doordashaustralia');
+
+          if (script == 'QUEUED') {
+            nlapiLogExecution('ERROR', 'Re-scheduling due to governance', 'Successful re-schedule.');
+            return true;
+          } else {
+            nlapiLogExecution('ERROR', 'Problem re-scheduling.', e.code + ' : ' + e.message);
             return true;
           }
-          else {
-            nlapiLogExecution('ERROR','Problem re-scheduling.', e.code+' : '+e.message);
-            return true;
-          }
-					
-				}
-				catch(e) {
-					nlapiLogExecution('ERROR','Problem re-scheduling.', e.code+' : '+e.message);
-					return true;
-				}
-			}
-			else {
-				return false;
-			}
 
-		};
+        } catch (e) {
+          nlapiLogExecution('ERROR', 'Problem re-scheduling.', e.code + ' : ' + e.message);
+          return true;
+        }
+      } else {
+        return false;
+      }
+
+    };
 
     //check if ALL of the items are backordered
-    var allBackordered = function(rec) {
-      for(var i=1; i<=rec.getLineItemCount('item'); i++) {
-        if(rec.getLineItemValue('item','quantitycommitted',i) > 0) {
+    /*****  REVIEW THIS   *******/
+    var allBackordered = function (rec) {
+      for (var i = 1; i <= rec.getLineItemCount('item'); i++) {
+        if (rec.getLineItemValue('item', 'quantitycommitted', i) > 0) {
           return false;
         }
       }
       return true;
     };
 
-    var dateConversion = function(date) {
+    var dateConversion = function (date) {
       //convert date to YYYY-MM-DDT00:00:00
       var dateObject = nlapiStringToDate(date);
       var dateMonth = null;
       var dateDay = null;
-      if((dateObject.getMonth()+1).toString().length < 2) {
-        dateMonth = '0'+(dateObject.getMonth()+1).toString();
+      if ((dateObject.getMonth() + 1).toString().length < 2) {
+        dateMonth = '0' + (dateObject.getMonth() + 1).toString();
+      } else {
+        dateMonth = (dateObject.getMonth() + 1).toString();
       }
-      else {
-        dateMonth = (dateObject.getMonth()+1).toString();
-      }
-      if((dateObject.getDate()).toString().length < 2) {
-        dateDay = '0'+(dateObject.getDate()).toString();
-      }
-      else {
+      if ((dateObject.getDate()).toString().length < 2) {
+        dateDay = '0' + (dateObject.getDate()).toString();
+      } else {
         dateDay = (dateObject.getDate()).toString();
       }
-      return dateObject.getFullYear()+'-'+dateMonth+'-'+dateDay+'T00:00:00';
+      return dateObject.getFullYear() + '-' + dateMonth + '-' + dateDay + 'T00:00:00';
     };
 
-    for(var i = 0; i<results.length; i++) {
+    for (var i = 0; i < results.length; i++) {
 
       try {
 
         var rec = null;
         var docNumber = null;
+
         var payload = {
-          CustomerCode: 'DD',	
-          CustomerName: '',
-          Status: '',
-          Warehouse: 'WH1',
-          Reference: 'REF',
-          OrderDate: '',
-          DateWanted: '',
-          JobReference: '',
-          CustomerReference: '',
-          CarrierCode: 'AUSPOST',
-          CarrierName: '',
-          TrackingNumber: '',
-          Receiver:{
-            Code: '',
-            Name: '',
-            Address: '',
-            Suburb: '',
-            StateCode: '',
-            PostCode: '',
-            CountryCode: '',
-            Telephone: '',
-            Email: ''
-          },
-          Instructions: '',
-          Notes: '',
-          CustomerOrderLines:[],
-          ExtraCharges:0
+          tranid: "",
+          internalid: "",
+          name: "",
+          email: "",
+          shipaddressee: "",
+          shipattention: "",
+          shipphone: "",
+          shipaddr1: "",
+          shipaddr2: "",
+          shipaddr3: "",
+          shipcity: "",
+          shipstate: "",
+          shipzip: "",
+          shipcountry: "",
+          items: []
+        }
+
+        var backorderedLines = {
+          lines: []
         };
 
-        //What am i doing? This seems wrong... yup it broke the server. 
-        //So these records are based of results going through this for loop. should I get rid of this and override?
-        //We are bulding json payloads so... I'm stilling getting 1000's of results based off the script.
-        //Should we get out of this loop? Or what are we sending since we have the send function is commented out. 
-        //God help me. 
-        var primaryPayload = {
-          "tranid": "SO3581790",
-          "internalid": "50110886",
-          "name": "Jacob Goodall",
-          "email": "jacobg@overturepromo.com",
-          "shipaddressee": "Overture Promotions Inc.",
-          "shipattention": "Jacob Goodall",
-          "shipphone": "8475736080",
-          "shipaddr1": "800 S Northpoint Blvd",
-          "shipaddr2": "",
-          "shipaddr3": "",
-          "shipcity": "Waukegan",
-          "shipstate": "IL",
-          "shipzip": "60085",
-          "shipcountry": "US",
-          "items": [
-            {
-              "sku": "DDJP001",
-              "quantity": "5"
-            },
-            {
-              "sku": "DDJP002",
-              "quantity": "1"
-            },
-            {
-              "sku": "DDJP003",
-              "quantity": "1"
-            }
-          ]
-        }
+        rec = nlapiLoadRecord('salesorder', results[i].getId());
 
-        var backorderedLines = {lines:[]};
-        
-        rec = nlapiLoadRecord('salesorder',results[i].getId());
-        
-        if(allBackordered(rec)) {
-          docNumber = rec.getFieldValue('tranid')+'-BO';
-          payload.CustomerReference = docNumber;
-        }
-        else {
+        //Do we need this? No customer reference in the payload. Maybe important
+        if (allBackordered(rec)) {
+          docNumber = rec.getFieldValue('tranid') + '-BO';
+          //payload.CustomerReference = docNumber;
+        } else {
           docNumber = rec.getFieldValue('tranid');
-          payload.CustomerReference = rec.getFieldValue('tranid');
-        }  
+          //payload.CustomerReference = docNumber;
+        }
 
-        payload.OrderDate = dateConversion(rec.getFieldValue('trandate'));
-        //don't have in-hands dates on these, so just using trandate
-        payload.DateWanted = dateConversion(rec.getFieldValue('trandate'));
-        //per BDL, need to put email in JobReference
-        payload.JobReference = rec.getFieldValue('custbody_customer_email');
-        payload.Receiver.Name = rec.getFieldValue('shipattention');
-        var shipAdd = rec.getFieldValue('shipaddr1');
-        if(rec.getFieldValue('shipaddr2')) {
-          shipAdd += '\r\n'+rec.getFieldValue('shipaddr2');
-        }
-        if(rec.getFieldValue('shipaddr3')) {
-          shipAdd += '\r\n'+rec.getFieldValue('shipaddr3');
-        }
-        payload.Receiver.Address = shipAdd;
-        
-        payload.Receiver.Suburb = rec.getFieldValue('shipcity');
-        payload.Receiver.StateCode = rec.getFieldValue('shipstate');
-        payload.Receiver.PostCode = rec.getFieldValue('shipzip');
-        payload.Receiver.CountryCode = rec.getFieldValue('shipcountry');
-        payload.Receiver.Telephone = rec.getFieldValue('custbody_shiptophone');
-        payload.Receiver.Email = rec.getFieldValue('custbody_customer_email');
-        if(rec.getFieldValue('custbody_special_instructions') == '; UPS Account:') {
-          payload.Instructions = '';
-        }
-        else {
-          payload.Instructions = rec.getFieldValue('custbody_special_instructions');
-        }
+        payload.tranid = rec.getFieldValue("tranid");
+        payload.internalid = rec.id;
+        payload.name = rec.getFieldValue("custbody_customer_name");
+        payload.email = rec.getFieldValue('custbody_customer_email');
+        payload.shipaddressee = rec.getFieldValue('shipaddressee');
+        payload.shipattention = rec.getFieldValue('shipattention');
+        payload.shipphone = rec.getFieldValue('custbody_shiptophone');
+        payload.shipaddr1 = rec.getFieldValue("shipaddr1");
+        payload.shipaddr2 = rec.getFieldValue("shipaddr2");
+        payload.shipaddr3 = rec.getFieldValue("shipaddr3");
+        payload.shipcity = rec.getFieldValue('shipcity');
+        payload.shipstate = rec.getFieldValue('shipstate');
+        payload.shipzip = rec.getFieldValue('shipzip');
+        payload.shipcountry = rec.getFieldValue('shipcountry');
         
 
-        for(var x=1; x<=rec.getLineItemCount('item'); x++) {
-          var item = rec.getLineItemText('item','item',x);
+        for (var x = 1; x <= rec.getLineItemCount('item'); x++) {
+          var item = rec.getLineItemText('item', 'item', x);
           //check if backordered
-          if(rec.getLineItemValue('item','quantitybackordered',x) < 1) {
+          if (rec.getLineItemValue('item', 'quantitybackordered', x) < 1) {
             //exclude DDHAU-FRT and DDH-FRT
-            if(rec.getLineItemValue('item','item',x) !== '337433' && rec.getLineItemValue('item','item',x) !== '215188'){
+            if (rec.getLineItemValue('item', 'item', x) !== '337433' && rec.getLineItemValue('item', 'item', x) !== '215188') {
               //check if matrix item, strip out parent item if so
-              if(item.indexOf(':') !== -1) {
-                item = item.substring(item.indexOf(':')+2);
+              if (item.indexOf(':') !== -1) {
+                item = item.substring(item.indexOf(':') + 2);
               }
-              payload.CustomerOrderLines.push(
-                {
-                  ProductCode: item,
-                  UOM: 'UNIT',
-                  QtyOrdered: Number(rec.getLineItemValue('item','quantitycommitted',x))
-                }
-              );
+              payload.items.push({
+                sku: item,
+                quantity: Number(rec.getLineItemValue('item', 'quantitycommitted', x))
+              });
             }
-            
-          }
-          else {
+
+          } else {
             //check if matrix item, strip out parent item if so
-            if(item.indexOf(':') !== -1) {
-              item = item.substring(item.indexOf(':')+2);
+            if (item.indexOf(':') !== -1) {
+              item = item.substring(item.indexOf(':') + 2);
             }
-            backorderedLines.lines.push(
-              {
-                ProductCode: item,
-                UOM: 'UNIT',
-                QtyOrdered: Number(rec.getLineItemValue('item','quantitybackordered',x))
-              }
-            );
+            backorderedLines.lines.push({
+              sku: item,
+              quantity: Number(rec.getLineItemValue('item', 'quantitybackordered', x))
+            });
           }
         }
 
-        if(payload.CustomerOrderLines.length > 0) {
+        if (payload.items.length > 0) {
 
-          rec.setFieldValue('custbody_ariba_cxml_message',JSON.stringify(payload));
-          nlapiLogExecution('AUDIT','Outbound Payload',JSON.stringify(payload));
+          rec.setFieldValue('custbody_ariba_cxml_message', JSON.stringify(payload));
+          nlapiLogExecution('AUDIT', 'Outbound Payload', JSON.stringify(payload));
 
-        //   var response = nlapiRequestURL(
-        //     'http://api-middleware.bdynamiclogistics.com.au/api/Orders',
-        //     JSON.stringify(payload),
-        //     header,
-        //     null,
-        //     'POST'
-        //   );
-  
-        //   var resCode = response.getCode();
-        //   var resBody = response.getBody();
-        //   var resString = resCode+'\n'+JSON.stringify(resBody);
-  
-        //   nlapiLogExecution('AUDIT','WMS Response',resString);
-  
-        //   if(resCode === 200) {
+          //   var response = nlapiRequestURL(
+          //     'http://api-middleware.bdynamiclogistics.com.au/api/Orders',
+          //     JSON.stringify(payload),
+          //     header,
+          //     null,
+          //     'POST'
+          //   );
 
-            rec.setFieldValue('custbody_outbound_processing_complete','T');
-  
-            //if there are backordered lines
-            //save them in custom field
-            if(backorderedLines.lines.length > 0) {
-              rec.setFieldValue('custbody_backordered_lines_pending','T');
-              rec.setFieldValue('custbody_backordered_lines_data', JSON.stringify(backorderedLines));
-              rec.setFieldValue('custbody_backorder_doc_number', rec.getFieldValue('tranid')+'-BO');
-            }
-  
-            nlapiSubmitRecord(rec);
-            nlapiLogExecution('AUDIT','SUCCESS',rec.getFieldValue('tranid')+' created successfully in WMS');
+          //   var resCode = response.getCode();
+          //   var resBody = response.getBody();
+          //   var resString = resCode+'\n'+JSON.stringify(resBody);
 
-        //   }
-  
-          //else if errors are present, log them and email kevind@overturepromo.com
-        //   else {
-  
-        //     nlapiLogExecution('ERROR','WMS Error',response.toString());
-  
-        //     nlapiSendEmail(
-        //       '6',
-        //       'kevind@overturepromo.com',
-        //       'WMS Doordash Australia Error '+docNumber,
-        //       docNumber+'\r\n'+resString,
-        //       null,
-        //       null,
-        //       null,
-        //       null
-        //     );
-            
-        //     //still need to submit record to store outbound payload
-        //     nlapiSubmitRecord(rec);
-  
-        //   }
-        }
-        //if no payload.items, all items are backordered
-        else {
-          rec.setFieldValue('custbody_backordered_lines_pending','T');
-          rec.setFieldValue('custbody_backordered_lines_data', JSON.stringify(backorderedLines));
-          rec.setFieldValue('custbody_backorder_doc_number', rec.getFieldValue('tranid')+'-BO');
+          //   nlapiLogExecution('AUDIT','WMS Response',resString);
+
+          //   if(resCode === 200) {
+
+          rec.setFieldValue('custbody_outbound_processing_complete', 'T');
+
+          //if there are backordered lines
+          //save them in custom field
+          if (backorderedLines.lines.length > 0) {
+            rec.setFieldValue('custbody_backordered_lines_pending', 'T');
+            rec.setFieldValue('custbody_backordered_lines_data', JSON.stringify(backorderedLines));
+            rec.setFieldValue('custbody_backorder_doc_number', rec.getFieldValue('tranid') + '-BO');
+          }
 
           nlapiSubmitRecord(rec);
-          nlapiLogExecution('AUDIT','All backorder order NOT sent.',rec.getFieldValue('tranid'));
+          nlapiLogExecution('AUDIT', 'SUCCESS', rec.getFieldValue('tranid') + ' created successfully in WMS');
 
+          //   }
+
+          //else if errors are present, log them and email kevind@overturepromo.com
+          //   else {
+
+          //     nlapiLogExecution('ERROR','WMS Error',response.toString());
+
+          //     nlapiSendEmail(
+          //       '6',
+          //       'kevind@overturepromo.com',
+          //       'WMS Doordash Australia Error '+docNumber,
+          //       docNumber+'\r\n'+resString,
+          //       null,
+          //       null,
+          //       null,
+          //       null
+          //     );
+
+          //     //still need to submit record to store outbound payload
+          //     nlapiSubmitRecord(rec);
+
+          //   }
         }
+        //if no payload.items, all items are backordered
+        // else {
+        //   rec.setFieldValue('custbody_backordered_lines_pending','T');
+        //   rec.setFieldValue('custbody_backordered_lines_data', JSON.stringify(backorderedLines));
+        //   rec.setFieldValue('custbody_backorder_doc_number', rec.getFieldValue('tranid')+'-BO');
 
-        if(i % 10 == 0) {
-          if(checkGovernance() == true) {
+        //   nlapiSubmitRecord(rec);
+        //   nlapiLogExecution('AUDIT','All backorder order NOT sent.',rec.getFieldValue('tranid'));
+
+        // }
+
+        if (i % 10 == 0) {
+          if (checkGovernance() == true) {
             break;
-          }	
+          }
         }
-      }
-
-      catch(e) {
-        var error = e.code+' :: '+e.message;
-        nlapiLogExecution('ERROR','Try / Catch Error',error);
+      } catch (e) {
+        var error = e.code + ' :: ' + e.message;
+        nlapiLogExecution('ERROR', 'Try / Catch Error', error);
         nlapiSendEmail(
           '6',
-          'kevind@overturepromo.com',
-          'WMS Doordash Australia Error Main Try/Catch Error',
+          'jacobg@overturepromo.com',
+          'WMS Doordash Error Error Main Try/Catch Error',
           error.toString(),
           null,
           null,
@@ -319,7 +259,5 @@ function sendSO() {
         );
       }
     }
-		//callBackorderScript();
-	}
+  }
 }
-
---
+++ no-backorder.js
@@ -0,0 +1,263 @@
+/*
+ *Author: Jacob Goodall
+ *Date: 08/31/2021
+ *Description: Scheduled Script to send Sales Orders to DoorDash Japan
+ */
+
+
+
+/*
+High Level
+-Load a saved search
+-Loop through results
+-Load records
+-Create payload, send to third party. 
+-Send Payload
+-Evaluate response from third party, handle accordingly.
+*/
+function sendSO() {
+
+    //define header object for nlapiRequestURL() call
+    var header = {
+      'Cache-Control': 'no-cache',
+      'Content-Type': 'application/json',
+      'Authorization': '9661B173-E407-4793-9BC3-E7896554DAE6'
+    };
+  
+    //DoorDash Japan Orders to Send via Integration
+    var results = nlapiSearchRecord('salesorder', 'customsearch4722');
+  
+    if (results) {
+  
+      var checkGovernance = function () {
+  
+        var governanceThreshold = 500;
+        var context = nlapiGetContext();
+  
+        if (context.getRemainingUsage() < governanceThreshold) {
+  
+          try {
+  
+            var script = nlapiScheduleScript('customscript_op_sched_doordashaustralia');
+  
+            if (script == 'QUEUED') {
+              nlapiLogExecution('ERROR', 'Re-scheduling due to governance', 'Successful re-schedule.');
+              return true;
+            } else {
+              nlapiLogExecution('ERROR', 'Problem re-scheduling.', e.code + ' : ' + e.message);
+              return true;
+            }
+  
+          } catch (e) {
+            nlapiLogExecution('ERROR', 'Problem re-scheduling.', e.code + ' : ' + e.message);
+            return true;
+          }
+        } else {
+          return false;
+        }
+  
+      };
+  
+      //check if ALL of the items are backordered
+      /*****  REVIEW THIS   *******/
+      var allBackordered = function (rec) {
+        for (var i = 1; i <= rec.getLineItemCount('item'); i++) {
+          if (rec.getLineItemValue('item', 'quantitycommitted', i) > 0) {
+            return false;
+          }
+        }
+        return true;
+      };
+  
+      var dateConversion = function (date) {
+        //convert date to YYYY-MM-DDT00:00:00
+        var dateObject = nlapiStringToDate(date);
+        var dateMonth = null;
+        var dateDay = null;
+        if ((dateObject.getMonth() + 1).toString().length < 2) {
+          dateMonth = '0' + (dateObject.getMonth() + 1).toString();
+        } else {
+          dateMonth = (dateObject.getMonth() + 1).toString();
+        }
+        if ((dateObject.getDate()).toString().length < 2) {
+          dateDay = '0' + (dateObject.getDate()).toString();
+        } else {
+          dateDay = (dateObject.getDate()).toString();
+        }
+        return dateObject.getFullYear() + '-' + dateMonth + '-' + dateDay + 'T00:00:00';
+      };
+  
+      for (var i = 0; i < results.length; i++) {
+  
+        try {
+  
+          var rec = null;
+          var docNumber = null;
+  
+          var payload = {
+            tranid: "",
+            internalid: "",
+            name: "",
+            email: "",
+            shipaddressee: "",
+            shipattention: "",
+            shipphone: "",
+            shipaddr1: "",
+            shipaddr2: "",
+            shipaddr3: "",
+            shipcity: "",
+            shipstate: "",
+            shipzip: "",
+            shipcountry: "",
+            items: []
+          }
+  
+          var backorderedLines = {
+            lines: []
+          };
+  
+          rec = nlapiLoadRecord('salesorder', results[i].getId());
+  
+          //Do we need this? No customer reference in the payload. Maybe important
+          if (allBackordered(rec)) {
+            docNumber = rec.getFieldValue('tranid') + '-BO';
+            //payload.CustomerReference = docNumber;
+          } else {
+            docNumber = rec.getFieldValue('tranid');
+            //payload.CustomerReference = docNumber;
+          }
+  
+          payload.tranid = rec.getFieldValue("tranid");
+          payload.internalid = rec.id;
+          payload.name = rec.getFieldValue("custbody_customer_name");
+          payload.email = rec.getFieldValue('custbody_customer_email');
+          payload.shipaddressee = rec.getFieldValue('shipaddressee');
+          payload.shipattention = rec.getFieldValue('shipattention');
+          payload.shipphone = rec.getFieldValue('custbody_shiptophone');
+          payload.shipaddr1 = rec.getFieldValue("shipaddr1");
+          payload.shipaddr2 = rec.getFieldValue("shipaddr2");
+          payload.shipaddr3 = rec.getFieldValue("shipaddr3");
+          payload.shipcity = rec.getFieldValue('shipcity');
+          payload.shipstate = rec.getFieldValue('shipstate');
+          payload.shipzip = rec.getFieldValue('shipzip');
+          payload.shipcountry = rec.getFieldValue('shipcountry');
+          
+  
+          for (var x = 1; x <= rec.getLineItemCount('item'); x++) {
+            var item = rec.getLineItemText('item', 'item', x);
+            //check if backordered
+            if (rec.getLineItemValue('item', 'quantitybackordered', x) < 1) {
+              //exclude DDHAU-FRT and DDH-FRT
+              if (rec.getLineItemValue('item', 'item', x) !== '337433' && rec.getLineItemValue('item', 'item', x) !== '215188') {
+                //check if matrix item, strip out parent item if so
+                if (item.indexOf(':') !== -1) {
+                  item = item.substring(item.indexOf(':') + 2);
+                }
+                payload.items.push({
+                  sku: item,
+                  quantity: Number(rec.getLineItemValue('item', 'quantitycommitted', x))
+                });
+              }
+  
+            } else {
+              //check if matrix item, strip out parent item if so
+              if (item.indexOf(':') !== -1) {
+                item = item.substring(item.indexOf(':') + 2);
+              }
+              backorderedLines.lines.push({
+                sku: item,
+                quantity: Number(rec.getLineItemValue('item', 'quantitybackordered', x))
+              });
+            }
+          }
+  
+          if (payload.items.length > 0) {
+  
+            rec.setFieldValue('custbody_ariba_cxml_message', JSON.stringify(payload));
+            nlapiLogExecution('AUDIT', 'Outbound Payload', JSON.stringify(payload));
+  
+            //   var response = nlapiRequestURL(
+            //     'http://api-middleware.bdynamiclogistics.com.au/api/Orders',
+            //     JSON.stringify(payload),
+            //     header,
+            //     null,
+            //     'POST'
+            //   );
+  
+            //   var resCode = response.getCode();
+            //   var resBody = response.getBody();
+            //   var resString = resCode+'\n'+JSON.stringify(resBody);
+  
+            //   nlapiLogExecution('AUDIT','WMS Response',resString);
+  
+            //   if(resCode === 200) {
+  
+            rec.setFieldValue('custbody_outbound_processing_complete', 'T');
+  
+            //if there are backordered lines
+            //save them in custom field
+            if (backorderedLines.lines.length > 0) {
+              rec.setFieldValue('custbody_backordered_lines_pending', 'T');
+              rec.setFieldValue('custbody_backordered_lines_data', JSON.stringify(backorderedLines));
+              rec.setFieldValue('custbody_backorder_doc_number', rec.getFieldValue('tranid') + '-BO');
+            }
+  
+            nlapiSubmitRecord(rec);
+            nlapiLogExecution('AUDIT', 'SUCCESS', rec.getFieldValue('tranid') + ' created successfully in WMS');
+  
+            //   }
+  
+            //else if errors are present, log them and email kevind@overturepromo.com
+            //   else {
+  
+            //     nlapiLogExecution('ERROR','WMS Error',response.toString());
+  
+            //     nlapiSendEmail(
+            //       '6',
+            //       'kevind@overturepromo.com',
+            //       'WMS Doordash Australia Error '+docNumber,
+            //       docNumber+'\r\n'+resString,
+            //       null,
+            //       null,
+            //       null,
+            //       null
+            //     );
+  
+            //     //still need to submit record to store outbound payload
+            //     nlapiSubmitRecord(rec);
+  
+            //   }
+          }
+          //if no payload.items, all items are backordered
+          // else {
+          //   rec.setFieldValue('custbody_backordered_lines_pending','T');
+          //   rec.setFieldValue('custbody_backordered_lines_data', JSON.stringify(backorderedLines));
+          //   rec.setFieldValue('custbody_backorder_doc_number', rec.getFieldValue('tranid')+'-BO');
+  
+          //   nlapiSubmitRecord(rec);
+          //   nlapiLogExecution('AUDIT','All backorder order NOT sent.',rec.getFieldValue('tranid'));
+  
+          // }
+  
+          if (i % 10 == 0) {
+            if (checkGovernance() == true) {
+              break;
+            }
+          }
+        } catch (e) {
+          var error = e.code + ' :: ' + e.message;
+          nlapiLogExecution('ERROR', 'Try / Catch Error', error);
+          nlapiSendEmail(
+            '6',
+            'jacobg@overturepromo.com',
+            'WMS Doordash Error Error Main Try/Catch Error',
+            error.toString(),
+            null,
+            null,
+            null,
+            null
+          );
+        }
+      }
+    }
+  }
